ce9a2cabb4a3fe523b8dfc0b4e575184d327bc20
ROBERTO IRIGOYEN L-BAISSON <ririlop@mdef.es>
2024-05-24 09:29:26 +0200

    Inicio counter.vue con composition

---
+++ src/views/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Counter view</h1>
+    <h3>{{ counter }}</h3>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(5);
+
+    // const increase = () => {
+    //     counter.value++
+    // } 
+    // const decrease = () => {
+    //     counter.value--
+    // } 
+
+    return {
+      counter,
+      increase: () => counter.value++,
+      decrease: () => counter.value--
+    };
+  },
+};
+</script>
+
+<style></style>
